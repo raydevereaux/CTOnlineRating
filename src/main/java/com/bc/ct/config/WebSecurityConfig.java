a6cf85fb852c4fd8149b3dcb426da7ceb32b26a8
Dan Oliver <danoliver@bc.com>
2015-09-03 16:33:34 +0000

    Experimenting with csrf

--- src/main/java/com/bc/ct/config/WebSecurityConfig.java
+++ src/main/java/com/bc/ct/config/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.bc.ct.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,6 +11,7 @@ import org.springframework.security.config.annotation.web.servlet.configuration.
 
 @Configuration
 @EnableWebMvcSecurity
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
